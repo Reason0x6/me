aadafaf44bb87d48704409ad73b440ecb146e119
Reason6 <vk2fgav@gmail.com>
2026-03-10 21:46:36 +1100

    player

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,4 +8,9 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./features/jazz-device/jazz-device.component').then((module) => module.JazzDeviceComponent),
   },
+  {
+    path: 'tp-7-youtube',
+    loadComponent: () =>
+      import('./features/tp7-youtube/tp7-youtube.component').then((module) => module.Tp7YoutubeComponent),
+  },
 ];
